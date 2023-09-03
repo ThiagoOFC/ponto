a1df0c21aad04f6ab67eb5d5a0c5d15e5c92a0af
Thiago Chaves <thiagofrancachaves@gmail.com>
2023-09-03 00:13:52 -0300

    export

--- ponto-front/vite.config.js
+++ ponto-front/vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0', // Isso faz o servidor ouvir em todas as interfaces
+    port: 5173, // Porta do projeto Vite
+  },
 })
